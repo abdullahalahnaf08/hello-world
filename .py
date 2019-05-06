ffa0408ba4a5c7b9b505f71c1e913e8a1623494b
abdullahalahnaf08 <45527587+abdullahalahnaf08@users.noreply.github.com>
2019-05-06 12:13:07 +0600

    Create .py

---
+++ .py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='*')
+
+@bot.event
+async def on_ready():
+print('Logged in as')
+print(Mod-mail)
+print(574801982053941258)
+print('------')
+
+@bot.command()
+async def greet(ctx)
+await ctx.send("Hello/Hi, how are you?")
+
+bot.run('<NTc0ODAxOTgyMDUzOTQxMjU4.XM_P5g.sNJo2Ez6yAwEf-dub3W99MbBgJw>')
